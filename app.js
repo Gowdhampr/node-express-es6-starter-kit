a75c13dc3b87da76055d3e0d182d27c1d42b898a
Gowdham Rajendran <prgowdham@gmail.com>
2021-05-14 19:20:42 +0530

    Node express babel es6 configuration

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import compression from "compression";
+import dotenv from "dotenv";
+import multiparty from "connect-multiparty";
+
+dotenv.config();
+
+// routes
+import rootRoute from "./routes";
+
+// initialize app
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ limit: "50mb" }));
+
+// parse application/json
+app.use(bodyParser.json({ limit: "50mb" }));
+
+// middleware for handling multipart/form-data
+app.use(multiparty());
+
+// cors support
+app.use(cors());
+
+// add compression
+app.use(compression());
+
+// logs
+app.use(logger("dev"));
+
+// cookie support
+app.use(cookieParser());
+
+// apply routes
+app.use("/", rootRoute);
+
+// Healthz Route
+app.get("/healthz", (req, res) => {
+    res.send("ok");
+});
+
+export default app;
